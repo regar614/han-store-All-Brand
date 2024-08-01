008892cfa961f0c1f0d1df753c25efd3d62d37eb
regar614 <mhmmdsiregar79@gmail.com>
2024-08-01 11:49:40 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<div class="row">
+		<div class="col-md-5">
+			<img src="image/home/place.png" width="470px">
+		</div>
+		<div class="col-md-7">
+			<h3>han_store</h3>
+			<p>.</p>
+		</div>
+	</div>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
